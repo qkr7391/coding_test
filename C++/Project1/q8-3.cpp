6499e42594ac602eb1a5309159139e3c0dbaafea
Sammy Park <saemipark7391@gmail.com>
2023-08-20 17:58:50 -0400

    coding test

--- C++/Project1/q8-3.cpp
+++ C++/Project1/q8-3.cpp
@@ -2,24 +2,8 @@
 
 using namespace std;
 
-	char q[5][15];
 int main() {
 
-	for (size_t i = 0; i < 5; i++)
-	{
-		cin >> q[i];
-	}
 
-	for (size_t i = 0; i < 15; i++)
-	{
-		for (size_t j = 0; j < 5; j++)
-		{
-			if (q[j][i] != NULL)
-			{
-				cout << q[j][i];
-			}
-		}
-	}
-	
 	return 0;
 }
